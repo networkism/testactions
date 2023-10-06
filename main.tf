859b380838d9366044c9ebffdb308d211d36a4e7
Networkism <omotayoolawale0@gmail.com>
2023-10-05 21:29:59 -0700

    Secrets

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {}
 
-resource "aws_vpc" test {
-	cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
 
-	tags = {
-		Name = "Actions-test"
-	}
+  tags = {
+    Name = "Actions-test"
+  }
 }
 
